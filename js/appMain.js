0ecf931705709a648fc6c32a861c8f542b524dc6
mehrdadj2005 <mehrdadjokari13@gmail.com>
2023-10-28 17:42:51 +0330

    create page landing

---
+++ js/appMain.js
@@ -0,0 +1,29 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+// Select button (next slide)
+let owlPrev = document.querySelector('.owl-prev span')
+
+document.addEventListener('DOMContentLoaded', nextSlide)
+
+// Show the next slide
+function nextSlide() {
+    // A button (next slide) is clicked every 3 seconds
+    setInterval(() => {
+        owlPrev.click()
+    }, 4000);
+}
