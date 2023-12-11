7c57d6f859006de22197c342562a17e49e479b8d
mehrdadj2005 <mehrdadjokari13@gmail.com>
2023-12-12 00:02:07 +0330

    create loading page
    1 = Create a loading page
    2 = Add a poster to the slider

--- about us-page/about-us.js
+++ about us-page/about-us.js
@@ -1,5 +1,10 @@
 let backToInformationPage = document.querySelector('#backToInformationPage')
 
+// At the time of entering the loading-page in the local storage, it should be equal to true
+window.addEventListener('DOMContentLoaded', () => {
+    localStorage.setItem('loading-page', 'true')
+})
+
 backToInformationPage.addEventListener('click', switchPageToSetting)
 
 function switchPageToSetting() {
--- setting page/setting.js
+++ setting page/setting.js
@@ -6,6 +6,11 @@ userInformation.addEventListener('click', switchPageToUserInformation)
 backToPageMain.addEventListener('click', switchPageToMain)
 goToPageAboutUs.addEventListener('click', switchPageToAboutUs)
 
+// At the time of entering the loading-page in the local storage, it should be equal to true
+window.addEventListener('DOMContentLoaded', () => {
+    localStorage.setItem('loading-page', 'true')
+})
+
 function switchPageToUserInformation() {
     window.location.href = "../user-information-page/information page.html"
 }
